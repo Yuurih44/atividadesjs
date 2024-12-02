db84723a0a495d5f9c3a4b46018f607842c38d5c
Yuri Siqueira <yurisiqueiraalves@gmail.com>
2024-12-02 17:18:42 -0300

    Add files via upload

---
+++ portifolio 2/PersonalPortifolio/scripts.js
@@ -0,0 +1,6 @@
+// Botão de enviar mensagem
+document.querySelector('.contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+  });
+  
