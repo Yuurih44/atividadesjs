63bd4ae1db32504abf9612210423f0241ae1be08
Yuri Siqueira <yurisiqueiraalves@gmail.com>
2024-12-02 17:40:06 -0300

    Delete portifolio 2 directory

--- portifolio 2/PersonalPortifolio/scripts.js
+++
@@ -1,6 +0,0 @@
-// Botão de enviar mensagem
-document.querySelector('.contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-  });
-  
