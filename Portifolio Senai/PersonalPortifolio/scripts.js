d138bc280482e0f33e4433e18e70f4cb1f5d8599
Yuri <yurisiqueiraalves@gmail.com>
2024-12-04 16:17:11 -0300

    Add files via upload

--- Portifolio Senai/PersonalPortifolio/scripts.js
+++ Portifolio Senai/PersonalPortifolio/scripts.js
@@ -1,9 +1,8 @@
 // Botão de enviar mensagem
 document.querySelector('.contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-  });
-  document.getElementById('theme-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-  });
-  
+  e.preventDefault();
+  alert('Mensagem enviada com sucesso!');
+});
+document.getElementById('theme-toggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+});
