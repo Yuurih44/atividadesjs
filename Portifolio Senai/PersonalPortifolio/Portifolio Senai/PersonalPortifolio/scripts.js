16e15f176a3c346e620d3af9131f9b83f47cb0d8
Yuri <yurisiqueiraalves@gmail.com>
2024-12-06 09:19:56 -0300

    Delete Portifolio Senai directory

--- Portifolio Senai/PersonalPortifolio/Portifolio Senai/PersonalPortifolio/scripts.js
+++
@@ -1,9 +0,0 @@
-// Botão de enviar mensagem
-document.querySelector('.contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-  });
-  document.getElementById('theme-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-  });
-  
